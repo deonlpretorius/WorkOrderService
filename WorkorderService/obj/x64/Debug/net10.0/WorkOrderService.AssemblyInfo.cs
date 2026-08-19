3f75389aa0209eb724bff52837acab6b35d2e1fc
Deon <deonlpretorius@outlook.com>
2026-08-19 16:16:28 +0200

    Changed the appsettings file connection string database name.

--- WorkorderService/obj/x64/Debug/net10.0/WorkOrderService.AssemblyInfo.cs
+++ WorkorderService/obj/x64/Debug/net10.0/WorkOrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkOrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66fb1e6160a8a9afb9a05389615c99a1d8e81870")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0249dff561a04bc62875c7d8b1e0d3d1fbd9a9f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkOrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkOrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
