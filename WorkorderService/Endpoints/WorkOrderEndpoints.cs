15bf6d5e4d3deb0ade527fd3699ca3fb5b972041
Deon <deonlpretorius@outlook.com>
2026-08-20 21:33:07 +0200

    Started with the Work Order Endpoint.

---
+++ WorkorderService/Endpoints/WorkOrderEndpoints.cs
@@ -0,0 +1,23 @@
+﻿using WorkOrderService.Models.WorkOrders;
+//using WorkOrderService.
+
+/// <summary>
+/// Namespace <c>WorkOrderService.Endpoints</c> contains the HTTP Endpoints and Route Groupings for the Work Order Service application.
+/// </summary>
+namespace WorkOrderService.Endpoints
+{
+    /// <summary>
+    /// Class <c>WorkOrderEndpoints</c> represents the Work Orders Endpoint.
+    /// </summary>
+    public static class WorkOrderEndpoints
+    {
+        public static void MapWorkOrderEndpoints(this IEndpointRouteBuilder routes)
+        {
+            // Makes use of Route Groups for a cleaner API.
+            var workOrderGroup = routes.MapGroup("/api/workorders")
+                                       .WithTags("WorkOrders");
+
+            // workOrderGroup.MapGet("/{workOrderId:string}", async (string WorkOrder, IWorkOrdersService ))
+        }
+    }
+}
