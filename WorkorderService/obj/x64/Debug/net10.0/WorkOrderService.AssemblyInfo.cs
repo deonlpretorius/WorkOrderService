d93bbce3d7f13e8ce2f6c2c233ca07516823c002
Deon <deonlpretorius@outlook.com>
2026-08-21 14:05:43 +0200

    Assembly info stuff again.

--- WorkorderService/obj/x64/Debug/net10.0/WorkOrderService.AssemblyInfo.cs
+++ WorkorderService/obj/x64/Debug/net10.0/WorkOrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkOrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9458b2698d97aae5d5323ea101936d6c385f38dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b269b46cfdf0474410234dfc6a5b0c2b86153a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkOrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkOrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
