b21ab4358d74117e6d348c4261d4a7a59065deff
Deon <deonlpretorius@outlook.com>
2026-08-25 23:06:57 +0200

    Getting started with the automated tests for the web api.

---
+++ DigitalTwin.WebAPI.AutomatedTests/SitesTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Namespace <c>DigitalTwin.WebAPI.AutomatedTests</c> contains the Web API automated test for the application.
+/// </summary>
+namespace DigitalTwin.WebAPI.AutomatedTests
+{
+    internal class SitesTests
+    {
+        
+    }
+}
