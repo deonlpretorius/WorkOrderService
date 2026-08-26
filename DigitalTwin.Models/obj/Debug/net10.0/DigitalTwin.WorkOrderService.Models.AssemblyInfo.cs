c2cf6a89d8f9df9159afbce4720da1ff411f6007
Deon <deonlpretorius@outlook.com>
2026-08-26 23:09:18 +0200

    Finalized the work order automated tests for the due date.

--- DigitalTwin.Models/obj/Debug/net10.0/DigitalTwin.WorkOrderService.Models.AssemblyInfo.cs
+++ DigitalTwin.Models/obj/Debug/net10.0/DigitalTwin.WorkOrderService.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalTwin.WorkOrderService.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71308cdaaa9723a5f9a91312033766f10e33b14a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+523444c00b78dc8a3443bfd2aece0dcc1fa55f1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalTwin.WorkOrderService.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalTwin.WorkOrderService.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
