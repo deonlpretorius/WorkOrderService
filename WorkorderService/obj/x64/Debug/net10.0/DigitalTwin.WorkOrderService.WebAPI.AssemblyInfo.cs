6ad7dcdbbe1ae25e0a9613383622057bc589ed68
Deon <deonlpretorius@outlook.com>
2026-08-26 20:48:54 +0200

    Another merge commit.

--- WorkorderService/obj/x64/Debug/net10.0/DigitalTwin.WorkOrderService.WebAPI.AssemblyInfo.cs
+++ WorkorderService/obj/x64/Debug/net10.0/DigitalTwin.WorkOrderService.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalTwin.WorkOrderService.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55262318df353195045897b50b993a56b6b110d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71308cdaaa9723a5f9a91312033766f10e33b14a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalTwin.WorkOrderService.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalTwin.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
